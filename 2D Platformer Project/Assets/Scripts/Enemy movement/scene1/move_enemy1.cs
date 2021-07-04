e1e05943a7aa97f27d14b3d90ee2de2aa3ef9a52
anoushkasud <anoushkasud725@gmail.com>
2021-07-05 01:36:10 +0530

    modified level 2,3,4

--- 2D Platformer Project/Assets/Scripts/Enemy movement/scene1/move_enemy1.cs
+++ 2D Platformer Project/Assets/Scripts/Enemy movement/scene1/move_enemy1.cs
@@ -8,14 +8,14 @@ public class move_enemy1 : MonoBehaviour
     bool moveRight = true;
     private void Update()
     {
-        if (transform.position.x > 13.4)
+        if (transform.position.x > 13.7)
             moveRight = false;
         if (transform.position.x < 11)
             moveRight = true;
 
         if (moveRight)
-            transform.position = new Vector2( transform.position.x + movespeed * Time.deltaTime, 0.78f);
+            transform.position = new Vector2( transform.position.x + movespeed * Time.deltaTime, 0.7f);
         else
-            transform.position = new Vector2(transform.position.x - movespeed * Time.deltaTime, 0.78f);
+            transform.position = new Vector2(transform.position.x - movespeed * Time.deltaTime, 0.7f);
     }
 }
